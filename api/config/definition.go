ce6573b51b18f795a73d957626dcaed569851ab9
kosuke-kawano <kosuke_kawano@cyberagent.co.jp>
2020-09-18 14:28:12 +0900

    viperの導入

---
+++ api/config/definition.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DB DBConfig `yaml:mysql`
+}
+
+type DBConfig struct {
+	User string `yaml:user`
+	Pass string `yaml:pass`
+	Host string `yaml:host`
+	Port string `yaml:port`
+	Database string `yaml:database`
+}
+
+func Load() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("api/config/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("read error: %s \n", err)
+	}
+
+	var cfg Config
+
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("server error: %s \n", err)
+	}
+
+	return &cfg, nil
+}
--- api/config/util.go
+++ api/config/util.go
@@ -4,9 +4,9 @@ import "os"
 
 // GetEnvString 環境変数があればそれを使いなければ指定した値を返す
 func GetEnvString(param string, defaultValue string) string {
-	env := os.Getenv(param)
-	if env != "" {
-			return env
-	}
-	return defaultValue
+       env := os.Getenv(param)
+       if env != "" {
+                       return env
+       }
+       return defaultValue
 }
